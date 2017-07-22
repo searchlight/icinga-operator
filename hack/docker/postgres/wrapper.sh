a34b71b0551cd3bc6d0e43c22c7953820c9a2ef4
tamal <tamal@appscode.com>
2017-07-21 22:58:03 -0700

    added-all

--- hack/docker/postgres/wrapper.sh
+++ hack/docker/postgres/wrapper.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 echo "Waiting for icinga configuration ..."
-until [ -f /srv/icinga2/config ] > /dev/null; do echo '.'; sleep 5; cat /srv/icinga2/config; done
-export $(cat /srv/icinga2/config | xargs)
+until [ -f /srv/icinga2/config.ini ] > /dev/null; do echo '.'; sleep 5; cat /srv/icinga2/config.ini; done
+export $(cat /srv/icinga2/config.ini | xargs)
 
 cp -r $PGDATA/../docker-entrypoint-initdb.d /docker-entrypoint-initdb.d
 
-exec docker-entrypoint.sh "$@"
+# exec docker-entrypoint.sh "$@"
+# https://superuser.com/a/176788/441206
+source docker-entrypoint.sh
