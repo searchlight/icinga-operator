a34101d823999ebe90ab09b95929859c48ec4693
shahriar <aerofoil.kite@gmail.com>
2017-08-22 11:04:21 +0600

    fix

--- test/e2e/pod_alert_test.go
+++ test/e2e/pod_alert_test.go
@@ -79,7 +79,7 @@ var _ = Describe("PodAlert", func() {
 				Should(HaveIcingaObject(IcingaServiceState{Ok: *rs.Spec.Replicas}))
 
 			By("Increase replica")
-			rs, err := f.UpdateReplicaSet(rs.ObjectMeta, func(in extensions.ReplicaSet) extensions.ReplicaSet {
+			rs, err := f.TryPatchReplicaSet(rs.ObjectMeta, func(in *extensions.ReplicaSet) *extensions.ReplicaSet {
 				in.Spec.Replicas = types.Int32P(3)
 				return in
 			})
@@ -118,7 +118,7 @@ var _ = Describe("PodAlert", func() {
 				Should(HaveIcingaObject(IcingaServiceState{Ok: *rs.Spec.Replicas}))
 
 			By("Decreate replica")
-			rs, err := f.UpdateReplicaSet(rs.ObjectMeta, func(in extensions.ReplicaSet) extensions.ReplicaSet {
+			rs, err := f.TryPatchReplicaSet(rs.ObjectMeta, func(in *extensions.ReplicaSet) *extensions.ReplicaSet {
 				in.Spec.Replicas = types.Int32P(1)
 				return in
 			})
