9d357f68d1744d0178e168512fea720086d95a23
shahriar <aerofoil.kite@gmail.com>
2018-01-18 16:51:17 +0600

    notify with user's comment

--- plugins/notifier/sms.go
+++ plugins/notifier/sms.go
@@ -14,13 +14,23 @@ const (
 )
 
 func RenderSMS(alert api.Alert, req *Request) string {
+	var message string
 	if strings.ToUpper(req.Type) == EventTypeAcknowledgement {
-		return fmt.Sprintf("Service [%s] for [%s] is in \"%s\" state.\nThis issue is acked.", alert.GetName(), req.HostName, req.State)
+		message = fmt.Sprintf("Service [%s] for [%s] is in \"%s\" state.\nThis issue is acked.", alert.GetName(), req.HostName, req.State)
 	} else if strings.ToUpper(req.Type) == EventTypeRecovery {
-		return fmt.Sprintf("Service [%s] for [%s] is in \"%s\" state.\nThis issue is recovered.", alert.GetName(), req.HostName, req.State)
+		message = fmt.Sprintf("Service [%s] for [%s] is in \"%s\" state.\nThis issue is recovered.", alert.GetName(), req.HostName, req.State)
 	} else if strings.ToUpper(req.Type) == EventTypeProblem {
-		return fmt.Sprintf("Service [%s] for [%s] is in \"%s\" state.\nCheck this issue in Icingaweb.", alert.GetName(), req.HostName, req.State)
+		message = fmt.Sprintf("Service [%s] for [%s] is in \"%s\" state.\nCheck this issue in Icingaweb.", alert.GetName(), req.HostName, req.State)
 	} else {
-		return fmt.Sprintf("Service [%s] for [%s] is in \"%s\" state.", alert.GetName(), req.HostName, req.State)
+		message = fmt.Sprintf("Service [%s] for [%s] is in \"%s\" state.", alert.GetName(), req.HostName, req.State)
 	}
+	if req.Comment != "" {
+		if req.Author != "" {
+			message = message + " " + fmt.Sprintf(`%s says "%s".`, req.Author, req.Comment)
+		} else {
+			message = message + " " + fmt.Sprintf(`Comment: "%s".`, req.Comment)
+		}
+	}
+
+	return message
 }
